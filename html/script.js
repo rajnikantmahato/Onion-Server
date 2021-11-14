ba5308e35b50bb84d4f619844e25aa262ece6d51
Rajni KAnt mahato <58804492+rajnikantmahato@users.noreply.github.com>
2021-11-14 14:03:04 +0530

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,6 +1,6 @@
 // PS! Replace this with your own channel ID
 // If you use this channel ID your app will stop working in the future
-const CLIENT_ID = 'YON9z583J6Mu97J5';
+const CLIENT_ID = 'xBiRf2ubQD10dcAI';
 
 const drone = new ScaleDrone(CLIENT_ID, {
   data: { // Will be sent out as clientData via events
